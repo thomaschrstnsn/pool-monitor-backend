45d1f6435534c753788ecaa5b149ec0c912636a6
Thomas Christensen <thomas@chrstnsn.dk>
2024-08-04 23:31:25 +0200

    setup postgres via devenv

--- devenv.nix
+++ devenv.nix
@@ -1,19 +1,51 @@
 { pkgs, lib, config, inputs, ... }:
 
+let
+  database_user = "pool_monitor";
+  database_password = "secret_pool_admin_password";
+in
 {
   dotenv.enable = true;
-  env.GREET = "POOL MONITOR - backend";
+  env.DATABASE_URL = "postgres://${database_user}:${database_password}@localhost/pool_monitor";
 
-  languages.rust.enable = true;
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = (lib.optionals
+    pkgs.stdenv.isDarwin
+    (with pkgs.darwin.apple_sdk; [
+      frameworks.SystemConfiguration
+    ])
+  ++ [ pkgs.git ]);
+
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
+  # banner source: https://patorjk.com/software/taag/#p=display&f=Impossible&t=Pool%0AMonitor
   enterShell = ''
-    hello
+    echo "         _          _            _            _                                             "
+    echo "        /\ \       /\ \         /\ \         _\ \                                           "
+    echo "       /  \ \     /  \ \       /  \ \       /\__ \                                          "
+    echo "      / /\ \ \   / /\ \ \     / /\ \ \     / /_ \_\                                         "
+    echo "     / / /\ \_\ / / /\ \ \   / / /\ \ \   / / /\/_/                                         "
+    echo "    / / /_/ / // / /  \ \_\ / / /  \ \_\ / / /                                              "
+    echo "   / / /__\/ // / /   / / // / /   / / // / /                                               "
+    echo "  / / /_____// / /   / / // / /   / / // / / ____                                           "
+    echo " / / /      / / /___/ / // / /___/ / // /_/_/ ___/\                                         "
+    echo "/ / /      / / /____\/ // / /____\/ //_______/\__\/                                         "
+    echo "\/_/       \/_________/ \/_________/ \_______\/                                             "
+    echo "        _   _         _            _              _        _            _            _      "
+    echo "       /\_\/\_\ _    /\ \         /\ \     _     /\ \     /\ \         /\ \         /\ \    "
+    echo "      / / / / //\_\ /  \ \       /  \ \   /\_\   \ \ \    \_\ \       /  \ \       /  \ \   "
+    echo "     /\ \/ \ \/ / // /\ \ \     / /\ \ \_/ / /   /\ \_\   /\__ \     / /\ \ \     / /\ \ \  "
+    echo "    /  \____\__/ // / /\ \ \   / / /\ \___/ /   / /\/_/  / /_ \ \   / / /\ \ \   / / /\ \_\ "
+    echo "   / /\/________// / /  \ \_\ / / /  \/____/   / / /    / / /\ \ \ / / /  \ \_\ / / /_/ / / "
+    echo "  / / /\/_// / // / /   / / // / /    / / /   / / /    / / /  \/_// / /   / / // / /__\/ /  "
+    echo " / / /    / / // / /   / / // / /    / / /   / / /    / / /      / / /   / / // / /_____/   "
+    echo "/ / /    / / // / /___/ / // / /    / / /___/ / /__  / / /      / / /___/ / // / /\ \ \     "
+    echo "\/_/    / / // / /____\/ // / /    / / //\__\/_/___\/_/ /      / / /____\/ // / /  \ \ \    "
+    echo "        \/_/ \/_________/ \/_/     \/_/ \/_________/\_\/       \/_________/ \/_/    \_\/    "
+    echo ""
+    echo ✅ DATABASE_URL=$DATABASE_URL
+    echo ""
   '';
 
   # https://devenv.sh/tests/
@@ -23,10 +55,16 @@
   '';
 
   # https://devenv.sh/services/
-  # services.postgres.enable = true;
+  services.postgres = {
+    enable = true;
+    listen_addresses = "0.0.0.0";
+    initialScript = ''
+      CREATE USER ${database_user} WITH PASSWORD '${database_password}';
+      CREATE DATABASE pool_monitor OWNER ${database_user};'';
+  };
 
   # https://devenv.sh/languages/
-  # languages.nix.enable = true;
+  languages.rust.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 use axum::{
+    extract::State,
     http::StatusCode,
+    response::{IntoResponse, Response},
     routing::{get, post},
     Json, Router,
 };
+use core::fmt;
 use serde::Deserialize;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
     tracing_subscriber::fmt::init();
+
+    let db_connection_str = std::env::var("DATABASE_URL").expect("env DATABASE_URL");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(20)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await;
+
+    if let Err(e) = pool {
+        tracing::error!("Failed to connect to database: {:?}", e);
+        return;
+    }
+    let pool = pool.expect("handled");
 
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
-        .route("/data", post(record_temperature));
+        .route("/data", post(record_temperature))
+        .with_state(pool);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -26,15 +45,22 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn record_temperature(Json(payload): Json<TempMessage>) -> (StatusCode, String) {
+async fn record_temperature(
+    State(pool): State<PgPool>,
+    Json(payload): Json<TempMessage>,
+) -> Result<(StatusCode, String), AppError> {
     // insert your application logic here
     tracing::info!("Received: {:?}", payload);
 
     if (payload.0).len() == 0 {
-        return (StatusCode::BAD_REQUEST, "No data received".to_string());
+        return Err(AppError::RequestError(RequestError::NoDataReceived));
     }
 
-    (StatusCode::CREATED, "Gotcha".to_string())
+    let resp = sqlx::query_scalar("select 'hello world from pg'::text")
+        .fetch_one(&pool)
+        .await?;
+
+    Ok((StatusCode::CREATED, resp))
 }
 
 #[derive(Deserialize, Debug)]
@@ -44,4 +70,39 @@ struct TempMessage(Vec<TempReading>);
 struct TempReading {
     addr: String,
     temp: f64,
+}
+
+#[derive(Debug)]
+enum RequestError {
+    NoDataReceived,
+}
+
+impl fmt::Display for RequestError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+enum AppError {
+    DatabaseError(sqlx::Error),
+    RequestError(RequestError),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        match self {
+            AppError::DatabaseError(err) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response()
+            }
+            AppError::RequestError(err) => {
+                (StatusCode::BAD_REQUEST, err.to_string()).into_response()
+            }
+        }
+    }
+}
+
+impl From<sqlx::Error> for AppError {
+    fn from(err: sqlx::Error) -> Self {
+        AppError::DatabaseError(err)
+    }
 }
